bdec1437f69b338e6e977ee7c51fbdd066dff025
Prasanta Santra <prasanta.gis@gmail.com>
2025-07-23 14:04:00 +0530

    merge conflicts changes

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 def addition(a,b,c):
         return a+b+c
 
-<<<<<<< HEAD
 def substraction(a,b,d):
         return a-b-d
-=======
-def substraction(a,b,e):
-        return a-b-e
->>>>>>> 4b88506558948ba07c2fa3a3c02ef8a7f01156a1
 
 def division (a,b):
         return a/b
